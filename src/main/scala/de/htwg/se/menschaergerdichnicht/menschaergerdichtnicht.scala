7fd87ce4060778b685d8acc29b64af509b364117
Tarik Ünlü <tarik-uenlue@gmx.de>
2017-11-10 13:59:55 +0100

    renamed project, package structure, build.sbt and main class

--- src/main/scala/de/htwg/se/menschaergerdichnicht/menschaergerdichtnicht.scala
+++ src/main/scala/de/htwg/se/menschaergerdichnicht/menschaergerdichtnicht.scala
@@ -1,8 +1,8 @@
-package de.htwg.se.yourgame
+package de.htwg.se.menschaergerdichnicht
 
-import de.htwg.se.yourgame.model.Player
+import de.htwg.se.menschaergerdichnicht.model.Player
 
-object YourGame {
+object menschaergerdichtnicht {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
     println("Hello, " + student.name)
