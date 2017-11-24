09c5a591e9fa54f46aa05b00986684dd02a58aff
re431sch <rene-schink@web.de>
2017-11-24 08:57:57 +0100

    Class Cell with test

--- src/main/scala/de/htwg/se/yourgame/YourGame.scala
+++ src/main/scala/de/htwg/se/yourgame/YourGame.scala
@@ -1,10 +1,14 @@
 package de.htwg.se.yourgame
 
-import de.htwg.se.yourgame.model.Player
+import de.htwg.se.yourgame.model.{Player,Cell}
 
 object YourGame {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
     println("Hello, " + student.name)
+    val cell = Cell(1)
+    println(cell.toString)
+    println(cell.isSet)
+
   }
 }
---
+++ src/main/scala/de/htwg/se/yourgame/model/Cell.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.yourgame.model
+
+case class Cell(value: Int) {
+
+  def isSet: Boolean = value != 0
+  override def toString: String = "value: " + value
+
+
+}
